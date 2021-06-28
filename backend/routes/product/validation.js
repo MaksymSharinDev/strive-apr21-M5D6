c27605b1512b4ea3ed6061715dc66dd3c4618243
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-28 15:00:36 +0200

    fix: products validation return the expected res

--- backend/routes/product/validation.js
+++ backend/routes/product/validation.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator"
+import {body, validationResult} from "express-validator"
 //     {
 //         "_id": "5d318e1a8541744830bef139", //SERVER GENERATED
 //         "name": "3310",  //REQUIRED
@@ -9,10 +9,9 @@ import { body } from "express-validator"
 //         "createdAt": "2019-07-19T09:32:10.535Z", //SERVER GENERATED
 //         "updatedAt": "2019-07-19T09:32:10.535Z", //SERVER GENERATED
 //     }
-export default [
-    body("name").exists().withMessage("Product name is a mandatory field!"),
-    body("description").exists().withMessage("Product description is a mandatory field!"),
-    body("brand").exists().withMessage("Product brand is a mandatory field"),
-    body("price").exists().withMessage("Product price is a mandatory field"),
-    body("category").exists().withMessage("Product category is a mandatory field"),
-]
+export default  [
+        body("name").exists().withMessage("Product name is a mandatory field!"),
+        body("description").exists().withMessage("Product description is a mandatory field!"),
+        body("brand").exists().withMessage("Product brand is a mandatory field"),
+        body("price").exists().withMessage("Product price is a mandatory field"),
+        body("category").exists().withMessage("Product category is a mandatory field")]
